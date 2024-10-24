76f54a6d8568ff6ebaa5869795652a02142d7132
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-10-23 19:01:19 -0600

    set up terraform using gcloud

---
+++ create_terraform_sa.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}' )
+else
+    echo ".env file not found! Please craeete it with the necessary environment variables."
+    exit 1
+fi
+
+# Enable Google Coud APIs needed for the project
+gcloud services enable iam.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable composer.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable bigquery.googleapis.com
+gcloud services enable storage.googleapis.com
+
+# Create the service account
+gcloud iam service-accounts create $TF_SERVICE_ACCOUNT_NAME \
+    --description="Service account for terraform." \
+    --display-name="$TF_SERVICE_ACCOUNT_DISPLAY_NAME"
+
+echo "Created service account $TF_SERVICE_ACCOUNT_NAME"
+
+# Assign roles to the service account
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$TF_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/storage.admin"
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$TF_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/bigquery.admin"
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$TF_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/composer.admin"
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$TF_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/run.admin"
+
+echo "Assigned roles to $TF_SERVICE_ACCOUNT_NAME"
+
+# Create and save service account key
+gcloud iam service-accounts keys create $TF_KEY_PATH \
+    --iam-account=$TF_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com
+
+echo "Service account create and key saved to $TF_KEY_PATH"
