c71e841413c180b6f0a7bae1c46f0f4bc355009a
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-10-28 11:25:19 -0600

    updated example_dag_test.py v2

--- airflow/dags/example_dag_test.py
+++ airflow/dags/example_dag_test.py
@@ -2,6 +2,6 @@ import internal_unit_testing
 
 
 def test_dag_import():
-    from . import example_dag_test.py
+    from . import example_dag_test
 
-    internal_unit_testing.assert_has_valid_dag(example_dag_test.py)
+    internal_unit_testing.assert_has_valid_dag(example_dag_test)
