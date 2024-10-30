64e7fa8b7dd14257be64df0a2ff8ac173e97ec22
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-10-30 15:27:05 -0600

    testing data download on composer

--- airflow/dags/chi_openData_to_gcs.py
+++ airflow/dags/chi_openData_to_gcs.py
@@ -32,6 +32,11 @@ default_args = {"owner": "duncanh", "depends_on_past": False, "retries": 1}
 
 EXECUTION_DATE = "{{ds_nodash}}"
 
+# Ensure TEMP_DIR exists
+if not os.path.exists(TEMP_DIR):
+    os.makedirs(TEMP_DIR)
+
+
 with DAG(
     "1.0_fetch_and_upload_CHI_data",
     default_args=default_args,
@@ -41,31 +46,38 @@ with DAG(
 ) as dag:
 
     def fetch_dataset(api_url, tmp_file_name, **kwargs):
+        logging.info(f"Starting to fetch data from {api_url}")
         response = requests.get(api_url)
+        logging.info(f"Response status code: {response.status_code}")
         if response.status_code == 200:
             file_path = os.path.join(TEMP_DIR, f"{tmp_file_name}.csv")
+            logging.info(f"Saving data to {file_path}")
             with open(file_path, "wb") as file:
                 file.write(response.content)
+            logging.info(f"Successfully saved data to {file_path}")
             return file_path
         else:
+            logging.error(
+                f"Failed to fetch data from {api_url}: {response.status_code}"
+            )
             raise Exception(
                 f"Failed to fetch data from {api_url}: {response.status_code}"
             )
 
-    def format_csv_to_parquet(csv_file_path):
-        try:
-            table = csv.read_csv(csv_file_path)
-            parquet_file_path = csv_file_path.replace(".csv", ".parquet")
-            pq.write_table(table, parquet_file_path)
-            print(f"Succesffully converted {csv_file_path} to {parquet_file_path}")
-            return parquet_file_path
-        except Exception as e:
-            print(f"Failed to convert {csv_file_path} to Parquet: {str(e)}")
+    # def format_csv_to_parquet(csv_file_path):
+    #     try:
+    #         table = csv.read_csv(csv_file_path)
+    #         parquet_file_path = csv_file_path.replace(".csv", ".parquet")
+    #         pq.write_table(table, parquet_file_path)
+    #         print(f"Succesffully converted {csv_file_path} to {parquet_file_path}")
+    #         return parquet_file_path
+    #     except Exception as e:
+    #         print(f"Failed to convert {csv_file_path} to Parquet: {str(e)}")
 
-    trigger_second_dag = TriggerDagRunOperator(
-        task_id="trigger_gcs_to_gcp",
-        trigger_dag_id="2.0_export_data_from_GCS_to_GCP",
-    )
+    # trigger_second_dag = TriggerDagRunOperator(
+    #     task_id="trigger_gcs_to_gcp",
+    #     trigger_dag_id="2.0_export_data_from_GCS_to_GCP",
+    # )
 
     for dataset_name, api_url in TRAFFIC_DATASETS.items():
         fetch_data = PythonOperator(
@@ -78,23 +90,23 @@ with DAG(
             },
         )
 
-        format_to_parquet = PythonOperator(
-            task_id=f"format_to_parquet_{dataset_name}",
-            python_callable=format_csv_to_parquet,
-            op_kwargs={
-                "csv_file_path": "{{ti.xcom_pull(task_ids='fetch_dataset_"
-                + dataset_name
-                + "')}}"
-            },
-        )
+        # format_to_parquet = PythonOperator(
+        #     task_id=f"format_to_parquet_{dataset_name}",
+        #     python_callable=format_csv_to_parquet,
+        #     op_kwargs={
+        #         "csv_file_path": "{{ti.xcom_pull(task_ids='fetch_dataset_"
+        #         + dataset_name
+        #         + "')}}"
+        #     },
+        # )
 
-        upload_parquet = LocalFilesystemToGCSOperator(
-            task_id=f"upload_{dataset_name}_parquet_to_gcs",
-            src="{{ti.xcom_pull(task_ids='format_to_parquet_" + dataset_name + "')}}",
-            dst=f"traffic_data/{dataset_name}/chi_traffic_{dataset_name}_{EXECUTION_DATE}.parquet",
-            bucket=BUCKET,
-        )
+        # upload_parquet = LocalFilesystemToGCSOperator(
+        #     task_id=f"upload_{dataset_name}_parquet_to_gcs",
+        #     src="{{ti.xcom_pull(task_ids='format_to_parquet_" + dataset_name + "')}}",
+        #     dst=f"traffic_data/{dataset_name}/chi_traffic_{dataset_name}_{EXECUTION_DATE}.parquet",
+        #     bucket=BUCKET,
+        # )
 
-        fetch_data >> format_to_parquet >> upload_parquet
+        fetch_data  # >> format_to_parquet >> upload_parquet
 
-    [upload_parquet for datset_name in TRAFFIC_DATASETS] >> trigger_second_dag
+    # [upload_parquet for datset_name in TRAFFIC_DATASETS] >> trigger_second_dag
